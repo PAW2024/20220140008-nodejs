46e3f0857a75c86055041c3c896c900a7057ff84
DzarZaki <dzarzaki2@gmail.com>
2024-10-17 12:49:04 +0700

    ini commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 4000;
 
+app.use('/todos',todoRoutes);
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => { 
     res.render('index');
