c2974ab9cd7f2eabc7b230a6f89e7bd21b58a68c
DzarZaki <dzarzaki2@gmail.com>
2024-10-30 20:22:28 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -22,8 +22,34 @@ router.post('/', (req, res) => {
     const newTodo = {
         id: todos.length +1,
         task: req.body.task,
-        completed: false
-    }
+        completed: false     
+    };
+router.delete('/:id', (req, res) => {
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) return res.status(404).json({message: 'tugas tidak ditemukan'});
+
+    const deletedTodo = todos.splice(todoIndex, 1)[0]; // Menghapus dan menyimpan todo yang dihapus
+    res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
 });
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+// Endpoint untuk memperbarui tugas
+router.put('/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+
+    if (!todo) {
+        return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+    }
+
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID-${todo.id} telah diperbarui`,
+        updatedTodo: todo
+    });
+});
+
 
 module.exports = router;
