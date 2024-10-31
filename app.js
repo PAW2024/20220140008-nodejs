79fa2598fab2bc13243baccf133eb549863b8416
DzarZaki <dzarzaki2@gmail.com>
2024-10-31 18:14:28 +0700

    dbcrud_008

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const todoRoutes = require('./routes/tododb.js');
 const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 4000;
+require('dotenv').config();
+const port = process.env.PORT;
 
-app.use('/todos',todoRoutes);
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => { 
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
 });
 
 app.listen(port, () => {
