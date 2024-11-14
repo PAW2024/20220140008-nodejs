1db440389d4412ae5fb4b2169a98b9312bc1e921
DzarZaki <dzarzaki2@gmail.com>
2024-11-14 11:45:27 +0700

    phpadmin

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const todoRoutes = require('./routes/tododb.js');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
+const db = require('./Database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
 
 
 app.use(express.json());
@@ -10,11 +13,25 @@ app.use(express.json());
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout: 'layouts/main-layout',
+    });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+    });
+});
+
+app.get('/todo-views', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 app.use((req, res) => {
